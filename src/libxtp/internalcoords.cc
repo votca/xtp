8d72bcb7e76c00e79ceb23a2e64fd81eeecb4e45
Pranav Madhikar <pmadhikar@gmail.com>
2018-08-31 14:31:22 +0200

    Add support for optional Auxiliary bonds to InternalCoords

--- src/libxtp/internalcoords.cc
+++ src/libxtp/internalcoords.cc
@@ -32,6 +32,43 @@ inline std::tuple<int, int, double> ClosestAtoms(const std::vector<int>& comp1,
     return std::make_tuple(closest.first, closest.second, min);
 }
 
+inline int ConnectAtomsWithin(const double& threshFactor,
+                              const std::vector<QMAtom*>& qmMolecule,
+                              BglGraph& bondGraph, Eigen::MatrixXd& bondMatrix){
+    int numAtoms = qmMolecule.size();
+    int numBonds = 0;
+
+    tools::Elements elements;
+
+    for (int i = 0; i < numAtoms; ++i){
+        auto atomI = qmMolecule[i];
+
+        const double iCovRad = elements.getCovRad(atomI->getType(), "bohr");
+        const tools::vec iPos = atomI->getPos();
+
+        for (int j = i+1; j < numAtoms; ++j){
+            auto atomJ = qmMolecule[j];
+
+            const double jCovRad = elements.getCovRad(atomJ->getType(), "bohr");
+            const tools::vec jPos = atomJ->getPos();
+
+            double thresh = threshFactor*(iCovRad + jCovRad);
+
+            double dist = abs(iPos - jPos);
+
+            if (dist < thresh){
+                bondMatrix(i,j) = dist;
+                bondMatrix(j,i) = dist;
+
+                boost::add_edge(i, j, bondGraph);
+                numBonds += 1;
+            }
+        }
+    }
+
+    return numBonds;
+}
+
 InternalCoords::InternalCoords(const Orbitals& orb):
     InternalCoords(orb, false){};
 
@@ -48,50 +85,29 @@ InternalCoords::InternalCoords(const std::vector<QMAtom*>& _qmm, const bool _wit
     // This code implements the algorithm described in
     // https://doi.org/10.1063/1.1515483
 
-    tools::Elements elements;
     bondMatrix = Eigen::MatrixXd::Zero(numAtoms, numAtoms);
+    tools::Elements elements;
     BglGraph bondGraph;
+
+    double threshFactor = 1.3;
+
+    if (withAuxiliary){
+        threshFactor = 2.5;
+    }
 
     // calculate the fabulous internal coordinates here
 
     // covalent bonds
-    for (int i = 0; i < numAtoms; ++i){
-        auto atomI = qmMolecule[i];
-
-        const double iCovRad = elements.getCovRad(atomI->getType(), "bohr");
-        const tools::vec iPos = atomI->getPos();
-
-        for (int j = i+1; j < numAtoms; ++j){
-            auto atomJ = qmMolecule[j];
-
-            const double jCovRad = elements.getCovRad(atomJ->getType(), "bohr");
-            const tools::vec jPos = atomJ->getPos();
-
-            double thresh = 1.3*(iCovRad + jCovRad);
-
-            double dist = abs(iPos - jPos);
-
-            if (dist < thresh){
-                bondMatrix(i,j) = dist;
-                bondMatrix(j,i) = dist;
-
-                boost::add_edge(i, j, bondGraph);
-                numBonds += 1;
-            }
-        }
-    }
-
-    // check the number of connected components
+    numBonds += ConnectAtomsWithin(threshFactor, qmMolecule, bondGraph, bondMatrix);
 
     // This part of the algorithm is a bit involved...
-
     // First we find the number of connected components
     // These are not necessarily real molecules in our case.
     // Rather, they are atoms that are within covalent range of each other
+    // times a factor.
     // Since this code is for calculating internal coordinates, we must only
     // have a single connected component in our graph.
     // This probably has some name in Graph theory, but I don't know it.
-
 
     // this will contain the component index that an atom belongs to.
     std::vector<int> idxInComponent(boost::num_vertices(bondGraph));
@@ -99,7 +115,6 @@ InternalCoords::InternalCoords(const std::vector<QMAtom*>& _qmm, const bool _wit
     int numComponents = boost::connected_components(bondGraph,
                                                     idxInComponent.data());
     possibleNumMols = numComponents;
-
 
     if (numComponents > 1){
         std::vector<std::vector<int>> components(possibleNumMols);
@@ -130,6 +145,17 @@ InternalCoords::InternalCoords(const std::vector<QMAtom*>& _qmm, const bool _wit
 
                 boost::add_edge(i, j, bondGraph);
                 numBonds += 1;
+
+                // auxiliary interfragment bonds if needed the paper
+                // says the threshold for this can be 2.0 ang or less
+                // than 1.3 times the interfragment distance. I'll go
+                // with the 1.3 factor...
+
+                if (withAuxiliary){
+                    numBonds += ConnectAtomsWithin(1.3*dist, qmMolecule,
+                                                   bondGraph, bondMatrix);
+                }
+
             }
 
         }
@@ -143,6 +169,8 @@ InternalCoords::InternalCoords(const std::vector<QMAtom*>& _qmm, const bool _wit
 
         // If everything was okay so far, we can proceed to calculate the
         // hydrogen bonds
+
+
     }
 
 
