ad2505b01d42e5262c5234cd3a0a92f83d52a21a
JoshuaSBrown <joshbro42867@yahoo.com>
2019-01-26 10:49:02 -0700

    Completed atom translate test

--- src/tests/test_atom.cc
+++ src/tests/test_atom.cc
@@ -79,6 +79,7 @@ BOOST_AUTO_TEST_CASE(getters_test) {
     vec quantum_chemistry_position(4.0, 5.0, 6.0);
     string element = "C";
     double weight = 12.01;
+
     Atom atom2(molecule_ptr, residue_name, residue_number,
                molecular_dynamics_atom_name, molecular_dynamics_atom_id,
                has_quantum_chemistry_part, quantum_chemistry_id,
@@ -104,6 +105,8 @@ BOOST_AUTO_TEST_CASE(setters_test) {
     vec quantum_chemistry_position(4.0, 5.0, 6.0);
     string element = "C";
     double weight = 12.01;
+    vec position(7.0, 8.0, 9.0);
+    bool has_position = true;
 
     Atom atom;
     atom.setResnr(residue_number);
@@ -111,6 +114,8 @@ BOOST_AUTO_TEST_CASE(setters_test) {
     atom.setWeight(weight);
     atom.setQMPart(quantum_chemistry_id, quantum_chemistry_position);
     atom.setElement(element);
+    atom.setPos(position);
+    atom.HasPos(has_position);
 
     atom.setTopology(nullptr);
     atom.setMolecule(nullptr);
@@ -123,6 +128,9 @@ BOOST_AUTO_TEST_CASE(setters_test) {
     BOOST_CHECK_EQUAL(atom.getElement(), element);
     BOOST_CHECK_EQUAL(atom.getQMId(), quantum_chemistry_id);
     BOOST_CHECK_EQUAL(atom.getQMPos(), quantum_chemistry_position);
+    BOOST_CHECK_EQUAL(atom.getPos(), position);
+    BOOST_CHECK(atom.HasPos());
+    BOOST_CHECK(atom.HasQMPart());
 
     BOOST_CHECK_EQUAL(atom.getTopology(), nullptr);
     BOOST_CHECK_EQUAL(atom.getMolecule(), nullptr);
@@ -131,11 +139,15 @@ BOOST_AUTO_TEST_CASE(setters_test) {
   }
 }
 
-BOOST_AUTO_TEST_CASE() {
+BOOST_AUTO_TEST_CASE(test_translateby) {
   Atom atom(1, "Ne");
 
   vec position(1.0, 2.0, 3.0);
   atom.setPos(position);
+
+  vec translation(1.0, 1.0, 1.0);
+  atom.TranslateBy(translation);
+  BOOST_CHECK_EQUAL(atom.getPos(), (position + translation));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
